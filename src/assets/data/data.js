fa79f645c584901524acdbc2046dad04c477c15b
TreleaniFederico <treleani.federico@gmail.com>
2022-10-05 16:25:56 +0200

    add data.js

---
+++ src/assets/data/data.js
@@ -0,0 +1,24 @@
+const headerData = [
+    Home, About, Services, Blog, Contact, Portfolio
+];
+
+const footerData = [
+    {
+        title: "Quick Links",
+        info: ["Services", "Contact", "Blog"]
+    },
+    {
+        title: "Resources",
+        info: ["Art Design", "Computer", "IT News", "Networking", "Web Security"]
+    },
+    {
+        title: "Contact Us",
+        info: [
+            {
+                Address: "Patricia C. Amedee 4401 Waldeck Street Grapevine Nashville, Tx 76051",
+                Phone: "+99 (0) 101 0000 888",
+                Email: "info@yourdomain.com"
+            }
+        ]
+    }
+];
