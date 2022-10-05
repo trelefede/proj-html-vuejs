e8b508fa362b36079a3ee3447ead2e64a6fb612c
TreleaniFederico <treleani.federico@gmail.com>
2022-10-05 12:25:24 +0200

    add heder, main and footer components import these in vue.app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
+    <HeaderComponent />
+    <MainComponent />
+    <FooterComponent />
   </div>
 </template>
 
 <script>
+import HeaderComponent from "@/components/HeaderComponent.vue"
+import MainComponent from "@/components/MainComponent.vue"
+import FooterComponent from "@/components/FooterComponent.vue"
 
 export default {
   name: 'App',
   components: {
-    
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   }
 }
 </script>
 
 <style lang="scss">
-@import '~bootstrap'
+@import '~bootstrap';
 
 </style>
