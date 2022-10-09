3f6e3fde228566b8f522f71e25ac27640c2f3c2a
TreleaniFederico <treleani.federico@gmail.com>
2022-10-09 12:32:56 +0200

    complete BlogCardComponent

---
+++ src/components/BlogCardComponent.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <div class="card ft-card" :class="card.id  === 0 ? 'card-large' : 'ft-bg-blue'">
+            <div class="card-body py-4 my-3" :class="card.id  === 0 ? 'card-large-body' : ''">
+                <div>
+                    <div class="heading d-flex">
+                        <p> {{ card.date }} </p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
+                            <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+                        </svg>
+                        <p>by {{ card.author }} </p>
+                    </div>
+                    <h5 class="my-3"> {{ card.title }} </h5>
+                    <p> {{ card.content }} </p>  
+                </div>
+                <div>
+                    <a class="btn rounded-pill ft-btn mt-2" href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BlogCardComponent",
+    props: {
+        card: Object
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.card-large{
+    background-image: url(@/assets/images/headway-537308-unsplash-1380x703.jpg);
+    background-size: cover;
+    background-position-x: right;
+    box-shadow: inset 0px 0px 20px 1000px rgb(18 15 15 / 50%) !important;
+    height: 100%;
+}
+
+.card-large-body{
+    flex-direction: row !important;
+    align-items: flex-end !important;
+    color: white;
+    > div{
+        max-width: 70%;
+    }
+    div :last-child{
+        margin-bottom: 19px;
+    }
+}
+
+.card{
+    height: 100%;
+}
+
+.card-body{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-around;
+    margin-left: 1rem;
+    margin-right: 1rem;
+    font-size: 0.8rem;
+}
+.ft-card{
+    border-radius: 1.75rem;
+    box-shadow: 1px -1px 70px 0px rgb(24 24 24 / 22%);
+    margin-top: 5rem;
+}
+.ft-btn{
+    color:#252525cc;
+    background-color: white;
+    font-size: 0.8rem;
+}
+
+h5{
+    font-size: 1.15rem;
+}
+
+p{
+    color: #cdcdcd;
+}
+</style>
--- src/components/BlogComponent.vue
+++ src/components/BlogComponent.vue
@@ -1,27 +1,48 @@
 <template>
-    <div>
+    <div class="container blog">
         <!-- heading section  -->
         <div class="row">
             <div class="col-12 text-center">
                 <p class="mt-4 mb-2 ft-color-red">Blog</p>
                 <h3 class="fw-light fs-1 mb-3"><span class="fw-bold">Latest</span> Posts</h3>
-                <p>When, while the lovely valley teems with vapour around meand the meridian sun strikes <br> the upper surface .</p>
+                <p id="heading-content">When, while the lovely valley teems with vapour around meand the meridian sun strikes <br> the upper surface .</p>
             </div>
         </div>
 
         <!-- content section create by data.js-->
         <div class="row">
-            <BlogCardComponent />
+            <BlogCardComponent :class="cardData.id === 0 ? 'col-6' : 'col-3'" 
+            v-for="cardData in blogData" :key="cardData.id" 
+            :card="cardData" />
         </div>
     </div>
 </template>
 
 <script>
+import BlogCardComponent from "@/components/BlogCardComponent.vue"
+
+import { blogData } from "@/data/data.js"
+
 export default {
     name: "BlogComponent",
+    data(){
+        return {
+            blogData
+        }
+    },
+    components: {
+        BlogCardComponent,
+    },
 }
 </script>
 
 <style lang="scss" scoped>
+.blog{
+    margin-top: 25rem;
+    margin-bottom: 7rem;
+}
 
+#heading-content{
+    color:#252525cc
+}
 </style>
